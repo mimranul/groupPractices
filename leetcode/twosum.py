b6290fd65cc3f67d411f63da4aeb3c597b9f9776
Imranul Islam <mimranul@gmail.com>
2023-01-07 15:02:40 +0600

    Changed format

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -1,13 +1,14 @@
 # URL: https://leetcode.com/problems/two-sum/discussion/
-from typing import List, Tuple
+from typing import List
 
 
-def twoSum(nums: List[int], target: int) -> tuple[int, int]:
+def twoSum(nums: List[int], target: int) -> List[int]:
     value = [0, 0]
     for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
+        for j in range(i + 1, len(nums)):
             if nums[j] == target - nums[i]:
                 value = [i, j]
     return value
 
-print(twoSum([0, 1, 2, 3, 4, 5], 2))
+
+print(twoSum([0, 1, 2, 3, 4, 6], 5))

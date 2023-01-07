569fdfc77f704e90b541bc5112388e55af3a30b5
Imranul Islam <mimranul@gmail.com>
2023-01-07 14:51:44 +0600

    Solution to two Sum updated

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -1,8 +1,13 @@
 # URL: https://leetcode.com/problems/two-sum/discussion/
-from typing import List
+from typing import List, Tuple
 
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    return 0, 0
+def twoSum(nums: List[int], target: int) -> tuple[int, int]:
+    value = [0, 0]
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[j] == target - nums[i]:
+                value = [i, j]
+    return value
 
-print(twoSum([0, 1, 2, 3, 4], 2))
+print(twoSum([0, 1, 2, 3, 4, 5], 2))

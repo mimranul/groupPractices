572b4139c1067d2084351b76099b7ba91d432712
Imranul Islam <mimranul@gmail.com>
2023-01-07 15:18:19 +0600

    Solution 2 Added

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -11,4 +11,22 @@ def twoSum(nums: List[int], target: int) -> List[int]:
     return value
 
 
-print(twoSum([0, 1, 2, 3, 4, 6], 5))
+def twoSumSolution2(nums: List[int], target: int) -> List[int]:
+    value = [0, 0]
+    hashmap = {}
+    for i in range(len(nums)):
+        hashmap[nums[i]] = i
+
+    for i in range(len(nums)):
+        compliment = target - nums[i]
+        if compliment in hashmap and hashmap[compliment] != i:
+            value = [hashmap[compliment], i]
+    print(hashmap)
+    return value
+
+
+given_list = [0, 1, 2, 3, 7, 6]
+target = 5
+
+print(twoSum(given_list, target))
+print(twoSumSolution2(given_list, target))
